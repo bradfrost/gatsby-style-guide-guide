21bfb7f3ece012fa014a2a67b83e50294ce426b9
Brad Frost <brad@bradfrost.com>
2018-04-27 10:44:02 -0600

    Syntax cleanup

--- src/components/header.js
+++ src/components/header.js
@@ -1,40 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Logo from "./logo";
 import Button from "./button";
 import PrimaryNav from "./primary-nav";
 
 export class Header extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {isToggleOn: true};
-		
-		this.toggleMenu = this.toggleMenu.bind(this);
-	}
+    constructor(props) {
+        super(props);
+        this.state = { isToggleOn: true };
+
+        this.toggleMenu = this.toggleMenu.bind(this);
+    }
 
     toggleMenu(e) {
         e.preventDefault();
         this.setState(prevState => ({
-			isToggleOn: !prevState.isToggleOn
-		}));
+            isToggleOn: !prevState.isToggleOn
+        }));
     }
 
     render() {
-		return (
-		<header className="c-header c-header--vertical" role="banner">
-            <div className="c-header__inner">
-                <Logo siteTitle={ this.props.siteTitle } />
-                <Button
-                    styleModifier="c-btn--small c-header__nav-btn"
-                    text={this.state.isToggleOn ? 'Menu' : 'Close'}
-                    onClickHandler={ this.toggleMenu }
-                />
-                <div className={this.state.isToggleOn ? 'c-header__nav-container' : 'c-header__nav-container is-active' }>
-                    <PrimaryNav />
+        return (
+            <header className="c-header c-header--vertical" role="banner">
+                <div className="c-header__inner">
+                    <Logo siteTitle={this.props.siteTitle} />
+                    <Button
+                        styleModifier="c-btn--small c-header__nav-btn"
+                        text={this.state.isToggleOn ? "Menu" : "Close"}
+                        onClickHandler={this.toggleMenu}
+                    />
+                    <div
+                        className={
+                            this.state.isToggleOn
+                                ? "c-header__nav-container"
+                                : "c-header__nav-container is-active"
+                        }
+                    >
+                        <PrimaryNav />
+                    </div>
                 </div>
-            </div>
-        </header>
-		)
-	}
-};
+            </header>
+        );
+    }
+}
 
 export default Header;
