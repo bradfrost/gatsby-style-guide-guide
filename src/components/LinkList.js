c7afef8aa1bdc987d7b874623f89f6fb9518c64d
Brad Frost <brad@bradfrost.com>
2018-06-11 10:56:28 -0400

    Remove ap= prefix

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -2,12 +2,12 @@ import React from "react";
 import Link from "gatsby-link";
 
 const LinkList = ({ items }) => (
-    <ul className="ap=c-link-list c-link-list--bulleted">
+    <ul className="c-link-list c-link-list--bulleted">
 
         {items.map(function(listItem) {
-            return <li className="ap=c-link-list__item">
-                <Link className="ap=c-link-list__link" to={ listItem.href }>
-                    <span className="ap=c-link-list__text">{ listItem.text }</span>
+            return <li className="c-link-list__item">
+                <Link className="c-link-list__link" to={ listItem.href }>
+                    <span className="c-link-list__text">{ listItem.text }</span>
                 </Link>
             </li>
         })}
