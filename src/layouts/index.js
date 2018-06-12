975d3f607e0cad0c9a2dddcb38e96451ee0d2b00
Brad Frost <brad@bradfrost.com>
2018-06-12 10:22:41 -0400

    Remove unused imports

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
-import Link from "gatsby-link";
 import Helmet from "react-helmet";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
