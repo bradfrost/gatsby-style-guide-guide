f159d8d168b7c9800921943abc706c73efff274b
Travis Reynolds <travis@cirruscreative.co.uk>
2018-09-18 17:16:40 +0100

    Added gatsby-prism

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,15 @@ module.exports = {
             options: {
               linkImagesToOriginal: false
             }
+          },
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+            },
           }
         ]
       }
